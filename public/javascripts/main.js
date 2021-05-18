d8a3bdf907685d73bba3a188e18006b0e4f73a80
Toni Halmetoja <36439969+ToniHalmetoja@users.noreply.github.com>
2021-05-18 13:01:40 +0200

    Styling, spacing
    nav/title bar, #game-container to append the actual game to side to side with the chat stuff. Plus soothing colors (I hope)

---
+++ public/javascripts/main.js
@@ -0,0 +1,74 @@
+const socket = io();
+
+document.getElementById('content').style.display= "none";
+
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+// GET USERNAME AND ROOM FROM URL
+
+document.getElementById('joinChat').addEventListener('click', () => {
+    document.getElementById('content').style.display= "flex";
+    document.getElementById('landingPage').style.display= "none";
+    const username = document.getElementById('username').value;
+
+    socket.emit('joinGame', username );
+});
+
+// GET ROOM AND USERS
+socket.on('roomUsers', ({ room, users }) => {
+    outputRoomName(room);
+    outputUsers(users);
+});
+
+// GET MESSAGE FROM SERVER
+socket.on('message', message => {
+    outputMessage(message);
+
+    // SCROLL DOWN
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+// SUBMIT MESSAGE TO SERVER
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const msg = e.target.elements.msg.value;
+    // const msg = document.getElementById('msg').value;
+
+    // SEND MESSAGE TO SERVER
+    socket.emit('chatMessage', msg);
+
+    // CLEAR INPUT
+    e.target.elements.msg.value = "";
+    e.target.elements.msg.focus();
+
+});
+
+// OUTPUT MESSAGE TO ALL USERS
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `
+    <p class="meta">${message.userName} <span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+};
+
+// PRINT ROOMNAME
+function outputRoomName(room) {
+    roomName.innerText = room;
+};
+
+function outputUsers(users) {
+    userList.innerHTML = "";
+
+    for (user in users) {
+        userList.insertAdjacentHTML('beforeend', `<li>${users[user].username}</li>
+    `);
+    };
+};
